5fc1197d5d002dc7788526e779ed9604f6b55b6f
Ochi Shuji <ochimms@ybb.ne.jp>
2020-04-07 00:54:40 +0900

    Modify love.py

--- love.py
+++ love.py
@@ -11,11 +11,8 @@ ims1,ims2 = [],[]
 for n in range(0,100):
     angle =  np.sin(2*np.pi*n/100)
     x_angle = x * angle
-    im1 = plt.plot(x_angle,f,color="red")
-    im2 = plt.plot(x_angle,g,color="red")
+    im1 = plt.plot(x_angle,f,x_angle,g,color="red")
     plt.xticks([-3,-2,-1,0,1,2,3])
     ims1.append(im1)
-    ims2.append(im2)
 ani1 = animation.ArtistAnimation(fig,ims1,interval = 50)
-ani2 = animation.ArtistAnimation(fig,ims2,interval = 50)
 plt.show()
